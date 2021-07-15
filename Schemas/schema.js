08ca55aa5787310025c0d5153dec175c4d26f89e
QuintusHoratiusFlaccus <konnor1122@gmail.com>
2021-07-15 14:16:48 +0300

    add enums| fix optionals, resolvers logic

--- Schemas/schema.js
+++ Schemas/schema.js
@@ -1,48 +1,60 @@
 const {gql} = require('apollo-server')
 
 const typeDefs = gql(`
+    enum DepositStatus {
+      INITIATED
+      FAILED
+      CONFIRMED
+      PROCESSING
+      CANCELLED
+      DECLINED
+    }
+    enum WithdrawalStatus {
+      INITIATED
+      FAILED
+      IN_PROGRESS
+      CANCELLED
+      PENDING
+      DECLINED
+      PROCESSING
+      CONFIRMED
+    }
 
     type User {
-        id: ID
-        username: String
-    }
-    
-    input UserInput {
-        id: ID
+        id: ID!
         username: String!
     }
     
     type Deposit {
-        id: ID
-        playerId: ID
-        status: String
-        currency: String
-        isLocked: Boolean
+        id: ID!
+        playerId: ID!
+        status: DepositStatus!
+        currency: String!
     }
     
     type Withdrawal {
-        id: ID
-        playerId: ID
-        status: String
-        currency: String
-        isLocked: Boolean
+        id: ID!
+        playerId: ID!
+        status: WithdrawalStatus!
+        currency: String!
+        isLocked: Boolean!
     }
     
     type Query {
-        getUserIdByName(name: String): User
-        getWithdrawal(
+        UserIdByName(name: String!): User
+        Withdrawals(
             id: ID
             playerId: ID
-            status: String
-            currency: String
+            status: [String]
+            currency: [String]
             isLocked: Boolean
-        ): [Withdrawal]!
-        getDeposit(
+        ): [Withdrawal!]!
+        Deposits(
             id: ID
             playerId: ID
-            status: String
-            currency: String
-        ): [Deposit]!
+            status: [String]
+            currency: [String]
+        ): [Deposit!]!
     }
 `)
 
--- resolvers.js
+++ resolvers.js
@@ -2,17 +2,26 @@ const USERS = require('./Database/UsersDB')
 const DEPOSITS = require('./Database/DepositsDB')
 const WITHDRAWALS = require('./Database/WithdrawalDB')
 
+const searchItems = (DB) => {
+    return (args) => {
+        const arrOfArgs = Object.entries(args)
+        return DB.filter(deposit => arrOfArgs.every((param) => {
+            if (Array.isArray(param[1])) {
+                return param[1].includes(deposit[param[0]])
+            }
+
+            return param[1] === deposit[param[0]]
+        }))
+    }
+}
+const DepositsDBBind = searchItems(DEPOSITS)
+const WithdrawalsDBBind = searchItems(WITHDRAWALS)
+
 const resolvers = {
     Query: {
-        getUserIdByName: (_, args) => USERS.find(user => args.name === user.username),
-        getDeposit: (_, args) => {
-            const arrOfArgs = Object.entries(args)
-            return DEPOSITS.filter(deposit => arrOfArgs.every((param) => param[1] === deposit[param[0]]))
-        },
-        getWithdrawal: (_, args) => {
-            const arrOfArgs = Object.entries(args)
-            return WITHDRAWALS.filter(deposit => arrOfArgs.every((param) => param[1] === deposit[param[0]]))
-        },
+        UserIdByName: (_, args) => USERS.find(user => args.name === user.username),
+        Withdrawals: (_, args) => WithdrawalsDBBind(args),
+        Deposits: (_, args) => DepositsDBBind(args),
     },
 }
 
